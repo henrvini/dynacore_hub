d7dfa2ec1976661b9f03c7d22927b8bf7c8dadd7
henrvini <vini0593@gmail.com>
2024-05-02 11:54:33 -0300

    utils creation

---
+++ src/utils/mock.jsx
@@ -0,0 +1,12 @@
+export const mockedData = {
+    freeMem: 4000000000,
+    totalMem: 12000000000,
+    usedMem: 16000000000,
+    memUsage: 0.5,
+    osType: 'windows',
+    upTime: 100,
+    cpuType: 'intel',
+    numCores: 6,
+    cpuSpeed: 1,
+    cpuLoad: 20
+};
